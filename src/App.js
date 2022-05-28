7bcc7a7bf383089d36074d02847652c7b9a7aaa9
Yusuf Yacoobali <yyacoobali@gmail.com>
2022-05-28 11:16:19 +0100

    build header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+
+//component is a func
+
+function Header(props){
+  return (
+    <header>
+      <h1>The {props.name} Group</h1>
+    </header>
+  )
+}
+
+function Main(){
+  return (
+    <section>
+      <p>We are the ultimate bloodline of warriors</p>
+    </section>
+  )
+}
+
+function Footer(){
+  return (
+    <footer>
+      <p>We are awake and dangerous</p>
+    </footer>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header name="Yacoobali"/>
+      <Main />
+      <Footer />
     </div>
   );
 }
